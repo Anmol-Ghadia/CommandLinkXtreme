28cffe4ced827a6335fcbb50457c18b709e4ed39
raghumanimehta <rmehta07@students.cs.ubc.ca>
2024-06-26 15:39:19 +0530

    added id store to chat.js and generate to join.js (front end)

--- public/script/chat.js
+++ public/script/chat.js
@@ -1,5 +1,6 @@
 const wsLink = 'ws://localhost:8080';
 let socket;
+let storedId = localStorage.getItem('sessionId');
 
 connectWS();
 
--- public/script/create.js
+++ public/script/create.js
@@ -1,6 +1,25 @@
 
 
-function goToChat() {
+async function goToChat() {
     // window.location.href = '/chat';
+    await storeId();
     window.location.href='/chat';
+}
+
+async function createSessionId() {
+    try {
+        const response = await fetch('create-session', {
+            method: 'POST'
+        });
+        const data = await response.json();
+        console.log('Session ID:', data.sessionId);
+        return data.sessionId;
+    } catch (error) {
+        console.error('Error fetching session ID:', error);
+    }
+}
+
+async function storeId() {
+    const sessionId = await createSessionId();
+    localStorage.setItem('sessionId', sessionId);
 }
